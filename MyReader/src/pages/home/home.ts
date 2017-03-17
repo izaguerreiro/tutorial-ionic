0ae17f2a09eea6679ba61bf7f676c92761f52882
Izabela Guerreiro <izaguerreiro@gmail.com>
2017-03-17 14:57:48 -0300

    First commit

---
+++ MyReader/src/pages/home/home.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController, LoadingController } from 'ionic-angular';
+import { InAppBrowser } from 'ionic-native';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public feeds: Array<string>;
+  private url: string = "https://www.reddit.com/new.json";  
+
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+    this.fetchContent();
+  }
+
+  fetchContent(): void {
+    let loading = this.loadingCtrl.create({
+      content: 'Fetching content...'
+    });
+ 
+    loading.present();
+ 
+    this.http.get(this.url).map(res => res.json()).subscribe(data => {
+      this.feeds = data.data.children;
+      loading.dismiss();
+    });  
+  }
+
+  itemSelected(url: string): void {
+    let browser = new InAppBrowser(url, '_system');
+  } 
+}
